07b906e0e4984742949a7575df0bbdb0a2096f5a
Roberto <roberto.p.gomes@hotmail.com>
2022-07-12 21:19:53 -0300

    Third class part2

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,55 +1,99 @@
+import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:job_timer/app/modules/home/controller/home_controller.dart';
 import 'package:job_timer/app/modules/home/widgtes/header_projects_menu.dart';
+import 'package:job_timer/app/view_models/project_model.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final HomeController controller;
+
+  const HomePage({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const Drawer(
-        child: SafeArea(
-          child: ListTile(
-            title: Text('Sair'),
+    return BlocListener<HomeController, HomeState>(
+      bloc: controller,
+      listener: (context, state) {
+        if (state.status == HomeStatus.failure) {
+          AsukaSnackbar.alert('Erro ao buscar os projetos').show();
+        }
+      },
+      child: Scaffold(
+        drawer: const Drawer(
+          child: SafeArea(
+            child: ListTile(
+              title: Text('Sair'),
+            ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            const SliverAppBar(
-              title: Text('Projetos'),
-              expandedHeight: 100,
-              toolbarHeight: 100,
-              centerTitle: true,
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(15)),
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                title: Text('Projetos'),
+                expandedHeight: 100,
+                toolbarHeight: 100,
+                centerTitle: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(15)),
+                ),
               ),
-            ),
-            SliverPersistentHeader(
-              delegate: HeaderProjectsMenu(),
-              pinned: true,
-            )
-          ],
+              SliverPersistentHeader(
+                delegate: HeaderProjectsMenu(),
+                pinned: true,
+              ),
+              BlocSelector<HomeController, HomeState, bool>(
+                bloc: controller,
+                selector: (state) => state.status == HomeStatus.loading,
+                builder: (context, showLoading) {
+                  return SliverVisibility(
+                    visible: showLoading,
+                    sliver: const SliverToBoxAdapter(
+                        child: SizedBox(
+                      height: 50,
+                      child: Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      ),
+                    )),
+                  );
+                },
+              ),
+              BlocSelector<HomeController, HomeState, List<ProjectModel>>(
+                bloc: controller,
+                selector: (state) => state.projects,
+                builder: (context, projects) {
+                  return SliverList(
+                    delegate: SliverChildListDelegate(projects
+                        .map((project) => ListTile(
+                              title: Text(project.name),
+                              subtitle: Text('${project.estimate}h'),
+                            ))
+                        .toList()),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 // ElevatedButton(
-          //     onPressed: () {},
-              // onPressed: () async {
-              //   final db = Modular.get<Database>();
+//     onPressed: () {},
+// onPressed: () async {
+//   final db = Modular.get<Database>();
 
-              //   final connection = await db.openConnection();
-              //   await connection.writeTxn((isar) => connection.clear());
-              //   connection.writeTxn((isar) {
-              //     var project = Project();
-              //     project.name = 'Projeto Teste';
-              //     project.status = ProjectStatus.em_andamento;
+//   final connection = await db.openConnection();
+//   await connection.writeTxn((isar) => connection.clear());
+//   connection.writeTxn((isar) {
+//     var project = Project();
+//     project.name = 'Projeto Teste';
+//     project.status = ProjectStatus.em_andamento;
 
-              //     return connection.projects.put(project);
-              //   });
-              // },
-              // child: const Text('Cadastrar'))
+//     return connection.projects.put(project);
+//   });
+// },
+// child: const Text('Cadastrar'))
